465a1728f81dc4bcd61b6e32c4fceb25ba320310
Reny Alvarado <renyalvarado@gmail.com>
2020-05-23 19:28:28 -0300

    Install Ansible using pip

--- ansible/start.sh
+++ ansible/start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -x
 
+echo "[servers]" > /etc/ansible/hosts
+
 for i in $(seq 1 ${MAX_ANSIBLE_NODES}); do
     sshpass -p "docker" ssh-copy-id -o StrictHostKeyChecking=no "sandboxdocker_ubuntussh_${i}"
+    echo "sandboxdocker_ubuntussh_${i}" >> /etc/ansible/hosts
 done
+
+echo "" >> /etc/ansible/hosts
+
 tail -f /dev/null
 
